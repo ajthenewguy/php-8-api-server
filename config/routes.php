b4d88854957c5afa44293c1357b2dd71b6c99aad
Allen McCabe <allenmccabe@gmail.com>
2021-08-23 10:06:32 -0700

    Update routes file.

--- config/routes.php
+++ config/routes.php
@@ -2,86 +2,46 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+use Ajthenewguy\Php8ApiServer\Http\Controllers;
 use Ajthenewguy\Php8ApiServer\Http\JsonResponse;
+use Ajthenewguy\Php8ApiServer\Http\Response;
 use Ajthenewguy\Php8ApiServer\Http\Request;
-use Ajthenewguy\Php8ApiServer\Models\User;
-use Ajthenewguy\Php8ApiServer\Repositories\UserRepository;
 use Ajthenewguy\Php8ApiServer\Routing\Guard;
 use Ajthenewguy\Php8ApiServer\Routing\Route;
-use Ajthenewguy\Php8ApiServer\Services\AuthService;
 
 /**
- * Define application routes below.
+ * Define application UI public routes below.
  */
 
 Route::get('/', function (Request $request) {
-    return JsonResponse::make("Hello there!\n");
+    return Response::make("Hello there!\n");
 });
 
-Route::post('/auth/register', function (Request $request) {
-    if ($request->contentType() !== 'application/json') {
-        return JsonResponse::make(['errors' => ['title' => 'Unsupported Media Type']], 415);
-    }
+/**
+ * Define application UI guarded routes below.
+ */
 
-    return $request->validate([
-        'email' => ['required', 'email', 'not-exists:users,email'],
-        'password' => ['required', 'string'],
-        'name_first' => ['required', 'string'],
-        'name_last' => ['required', 'string']
-    ], [
-        'email.not-exists' => 'An account with that email address already exists.'
-    ])->then(function ($validated) {
-        [$data, $errors] = $validated;
-        if ($errors) {
-            return JsonResponse::make(['errors' => $errors], 422);
-        }
-
-        $Repo = new UserRepository();
-
-        return $Repo->create($data)->then(function ($User) {
-            return JsonResponse::make(['data' => [
-                'type' => 'User',
-                'id' => $User->id
-            ]], 201);
-        });
+Route::get('/account', function (Request $request) {
+    return $request->user()->then(function ($User) {
+        return Response::make(sprintf("Welcome %s.\n", $User->name_first));
     });
+}, new Guard());
+
+
+/**
+ * Define application API public routes below.
+ */
+
+Route::get('/ping', function (Request $request) {
+    return JsonResponse::make("PONG\n");
 });
 
-Route::post('/auth/login', function (Request $request) {
-    if ($request->contentType() !== 'application/json') {
-        return JsonResponse::make(['errors' => ['title' => 'Unsupported Media Type']], 415);
-    }
+Route::post('/auth/register', new Controllers\Auth\RegisterController());
+Route::post('/auth/login', new Controllers\Auth\LoginController());
+Route::post('/auth/verify', new Controllers\Auth\VerifyController());
 
-    $validated = $request->validate([
-        'email' => ['required', 'email'],
-        'password' => ['required', 'string']
-    ]);
+/**
+ * Define application API guarded routes below.
+ */
 
-    return $validated->then(function ($validation) {
-        [$data, $errors] = $validation;
-        if ($errors) {
-            return JsonResponse::make(['errors' => $errors], 422);
-        }
-
-        return User::where('email', $data['email'])->first()->then(function ($User) use ($data) {
-            if (AuthService::authenticate($User, $data['password'])) {
-                $App = Ajthenewguy\Php8ApiServer\Application::singleton();
-                $tokenLifetime = $App->config()->get('security.tokenLifetime');
-                $claims = new \stdClass();
-                $claims->exp = time() + (60 * $tokenLifetime);
-                $claims->iat = time();
-                $claims->email = $User->email;
-
-                $token = Ajthenewguy\Php8ApiServer\Auth\Jwt::createToken($claims);
-
-                return JsonResponse::make([
-                    'access_token' => $token,
-                    'token_type' => 'Bearer',
-                    'expires_in' => $claims->exp - time()
-                ]);
-            } else {
-                return JsonResponse::make(['errors' => ['authentication' => 'Invalid email or password.']], 401);
-            }
-        });
-    });
-});
+Route::get('/auth/refresh', new Controllers\Auth\RefreshController(), new Guard());
