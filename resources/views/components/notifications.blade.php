7e2e928929546ec8f3bc766a0a0fd5717e7e8814
Allen McCabe <allenmccabe@gmail.com>
2021-08-25 23:15:15 -0700

    Fix notifications template.

--- resources/views/components/notifications.blade.php
+++ resources/views/components/notifications.blade.php
@@ -1,6 +1,6 @@
 <div class="has-text-left">
-@if(isset($session['errors']))
-    @foreach($session['errors'] as $field => $errors)
+@if(Session()->has('errors'))
+    @foreach(Session()->get('errors') as $field => $errors)
         @foreach($errors as $error)
             <div class="notification is-danger">
                 <!-- <button class="delete"></button> -->
